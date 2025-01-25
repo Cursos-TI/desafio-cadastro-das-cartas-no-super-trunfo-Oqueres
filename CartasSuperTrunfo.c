cce15d38d29e2beb577c1e6cba8d88e9a573e3ec
Oqueres <oqueresitalo@gmail.com>
2025-01-25 14:59:40 +0000

    SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,103 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+typedef struct
+{
+    char id_card;          // Identificador do card (primeira letra do estado)
+    char state[50];        // Nome do estado
+    char nome_cidade[50];  // Nome da cidade
+    int population;        // População da cidade
+    long int pib;          // PIB da cidade
+    int area;              // Área da cidade (em km²)
+    int spot_tourist;      // Quantidade de pontos turísticos
+    double pib_per_capita; // PIB per capita da cidade
+    double densidade;      // Densidade populacional da cidade
+} card;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+//Funçao para imprimir os dados de um card.
+void imprimir_card(card c)
+{
+    c.pib_per_capita = (double)c.pib / c.population;
+    c.densidade = (double)c.population / c.area;
+
+    printf(
+        "CARD: %c\n"
+        "ESTADO: %s\n"
+        "NOME DA CIDADE: %s\n"
+        "POPULAÇAO: %d\n"
+        "PIB: %ld\n"
+        "AREA: %d\n"
+        "PONTOS TURISTICOS: %d\n"
+        "DENSIDADE: %.2f\n"
+        "PIB PER CAPITA: %.2f\n",
+        c.id_card, c.state, c.nome_cidade, c.population, c.pib, c.area, c.spot_tourist, c.densidade, c.pib_per_capita);
+};
+
+//Funçao para cadastrar um novo card.
+void newCard(card *cards, int *last_position)
+{
+    card c;
+    printf("Digite o Estado: \n");
+    scanf("%s", c.state);
+    printf("Digite o Nome da cidade: \n");
+    scanf("%s", c.nome_cidade);
+    printf("Digite a populaçao: \n");
+    scanf("%d", &c.population);
+    printf("Digite o pib: \n");
+    scanf("%ld", &c.pib);
+    printf("Digite o area: \n");
+    scanf("%d", &c.area);
+    printf("Digite a quantidade de pontos turisticos: \n");
+    scanf("%d", &c.spot_tourist);
+    c.id_card = c.state[0];
+
+    cards[*last_position] = c; // Armazenando o card no vetor.
+    (*last_position)++;        // Atualiza a posiçao para o proximo card.
+};
+
+void listarCards(card *cards, int last_position)
+{
+    for (int i = 0; i < last_position; i++)
+    {
+        imprimir_card(cards[i]);
+    }
+}
+
+int main()
+{
+    card cards[50];
+    int last_position = 0;
+    int opcao = 0;
+
+    while (opcao != 3)
+    {
+        printf(
+            "1-Cadastrar Card: \n"
+            "2-Listar Cards: \n"
+            "3-Sair do programa. \n"
+            "Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+
+        case 1:
+            newCard(cards, &last_position); // Cadastrar um novo card.
+            break;
+
+        case 2:
+            listarCards(cards, last_position); // Listar todos os cards.
+            break;
+
+        case 3:
+            printf("Saindo do Programa... \n");
+            break;
+
+        default:
+            printf("Opcao Invalida! \n");
+            break;
+        };
+    };
 
     return 0;
-}
+};
